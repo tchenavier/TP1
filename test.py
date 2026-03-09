b7a9e572d7b0a0254455c254b6d2aeb907811415
tchenavier <thibaud.chenavier@gmail.com>
2026-03-09 14:10:10 +0100

    code de base

---
+++ test.py
@@ -0,0 +1,15 @@
+# Mini test : graphique seaborn
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+print ("moddule importer avec succes")
+# Mini test : graphique seaborn 
+data = pd.DataFrame({ 
+    "vitesse": np.random.normal(80, 15, 100), 
+    "danger": np.random.choice([0, 1], 100) 
+}) 
+sns.boxplot(x="danger", y="vitesse", data=data) 
+plt.title("Exemple graphique : vitesse selon danger") 
+plt.show() 
