c65dd40610f2eceff25c122217f8e27609691140
tchenavier <thibaud.chenavier@gmail.com>
2026-03-09 14:47:47 +0100

    preparation a l'ajout de jeu de donner

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
+df = pd.read_csv("Donner.csv")
 print ("moddule importer avec succes")
 # Mini test : graphique seaborn 
-data = pd.DataFrame({ 
+data = df({ 
     "vitesse": np.random.normal(80, 15, 100), 
-    "danger": np.random.choice([0, 1], 100) 
+    "danger": np.random.choice(["Safe", "danger"], 100) 
 }) 
 sns.boxplot(x="danger", y="vitesse", data=data) 
 plt.title("Exemple graphique : vitesse selon danger") 
